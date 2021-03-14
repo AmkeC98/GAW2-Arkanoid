b9b6bb667a8119cc11e1e7b03b625485b9f23de2
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-03-14 21:24:55 +0100

    Adds a main menu scene

---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("START GAME");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT GAME");
+        Application.Quit();
+    }
+}
