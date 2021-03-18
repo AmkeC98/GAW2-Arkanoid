8a40f0626b80f851a1e29ca130e71dd4d96f9f0e
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-03-18 17:04:16 +0100

    Adds mainmenu-mousehover interactivity

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour
+{
+    private Text buttonText;
+
+    private void Start()
+    {
+        buttonText = GetComponentInChildren<Text>();
+        buttonText.color = Color.green;
+    }
+
+    public void ChangeColorEnter()
+    {
+        buttonText.color = Color.white;
+    }
+
+    public void ChangeColorExit()
+    {
+        buttonText.color = Color.green;
+    }
+}
