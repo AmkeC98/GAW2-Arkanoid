676a4390e918aae8cbdd444515d605da5dea8af2
Amke Dijkstra <amkedijkstra@hotmail.com>
2021-03-22 11:20:17 +0100

    Adds score-text, adds gamescene, introduces bug with blocks

---
+++ Assets/Scripts/ScoreManagement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManagement : MonoBehaviour
+{
+    public Text scoreText;
+    private float score = 0;
+
+    private void Start()
+    {
+        setScore(0);
+    }
+
+    private void setScore(float scoreCount)
+    {
+        score += scoreCount;
+        scoreText.GetComponent<Text>().text = "Score: " + score.ToString();
+    }
+}
